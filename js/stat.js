90063c8ac930b3959317508aac98ba2d92193022
irina <irina.ryabova@webpp.ru>
2018-04-03 18:39:26 +0300

    Второе задание

---
+++ js/stat.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx, names, times) {
+  renderCloud(ctx, 100, 10, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 90, 0, '#fff');
+
+};
